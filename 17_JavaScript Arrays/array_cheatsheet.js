4a93c1d8da90f410e573a7134045cd3b1340f4cb
Jaideep <guntupallijaideep@gmail.com>
2022-01-25 14:51:52 +0530

    commit

---
+++ 17_JavaScript Arrays/array_cheatsheet.js
@@ -0,0 +1,31 @@
+// Making an array:
+const colors = ["red", "orange", "yellow"];
+
+// Arrays are indexed like strings:
+colors[0]; // "red"
+
+// They have a length:
+colors.length; //3
+
+// Important array methods:
+//push(value) - adds value to the END of an array
+//pop() - removes and returns last value in array
+
+//unshift(val) - adds value to START of an array
+//shift() - removes and returns first element in an array
+
+//indexOf(val) - returns index of first element in array that matches val
+//lastIndexOf(val) - returns index of last element in array that matches val
+
+//concat(array) - returns new array with all elements of both arrays
+
+//includes(val) - returns true if array contains val
+
+//reverse() - reverses order of elements in array
+
+//slice(start, end) - returns new array with elements from start to end
+//slice(start) - returns new array with elements from start to end of original array
+//splice(start, deleteCount, val1, val2, ...) - returns new array with elements from start to end
+//splice(start, deleteCount) - returns new array with elements from start to end
+//splice(start) - returns new array with elements from start to end
+//sort() - sorts array alphabetically
