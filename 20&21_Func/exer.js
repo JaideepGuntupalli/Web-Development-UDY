96495eb0ca9704d6203c4af1e88927cece28a372
Jaideep <guntupallijaideep@gmail.com>
2022-01-26 11:07:11 +0530

    commit change

---
+++ 20&21_Func/exer.js
@@ -0,0 +1,17 @@
+const square = {
+    area(side){
+        return side*side;
+    },
+    perimeter(side){
+        return side * 4;
+    }
+}
+
+const hen = {
+    name: "Helen",
+    eggCount: 0,
+    layAnEgg(){
+        this.eggCount++;
+        return "EGG";
+    }
+}
