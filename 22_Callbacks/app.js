63412313a33bfcaf1f3693c335fe5776ebd1a150
Jaideep <guntupallijaideep@gmail.com>
2022-01-26 16:18:09 +0530

    practice

---
+++ 22_Callbacks/app.js
@@ -0,0 +1,15 @@
+//  const nums = [1,2,3,4,5,6,7];
+
+// const doubs = nums.map(ele => {
+//     return ele*2;
+// })
+
+// console.log(doubs);
+
+const add = (x,y) => {
+    return x+y;
+}
+
+console.log(add(1,3));
+console.log(add(6,6));
+console.log(add(7,3));
---
+++ 22_Callbacks/exer.js
@@ -0,0 +1,15 @@
+const fullNames = [
+    { first: "Albus", last: "Dumbledore" },
+    { first: "Harry", last: "Potter" },
+    { first: "Hermione", last: "Granger" },
+    { first: "Ron", last: "Weasley" },
+    { first: "Rubeus", last: "Hagrid" },
+    { first: "Minerva", last: "McGonagall" },
+    { first: "Severus", last: "Snape" },
+];
+
+const firstNames = fullNames.map(name => {
+    return name.first;
+})
+
+console.log(firstNames);
