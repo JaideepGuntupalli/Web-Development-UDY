5e094ed4ceee5f73adb6986957b43d5d0f29145f
Jaideep <guntupallijaideep@gmail.com>
2022-02-17 11:19:44 +0530

    test

--- 27_Async/CreatingPromises/app.js
+++ 27_Async/CreatingPromises/app.js
@@ -53,7 +53,6 @@ const delayedColorChange = (color, delay) => {
     })
 }
 
-
 delayedColorChange('red', 1000)
     .then(() => delayedColorChange('orange', 1000))
     .then(() => delayedColorChange('yellow', 1000))
