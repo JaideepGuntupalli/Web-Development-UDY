baa22f6a6f130f82e70bc03b860ed74dbe899eef
Jaideep <guntupallijaideep@gmail.com>
2022-02-26 20:16:50 +0530

    update

--- 33_Express/first_app/index.js
+++ 33_Express/first_app/index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 // app.use((req, res) => {
@@ -6,30 +6,32 @@ const app = express();
 //     res.send("We got your request! This is the response");
 // });
 
-app.get('/r/:subreddit',(req,res) => {
+app.get("/r/:subreddit", (req, res) => {
     const { subreddit } = req.params;
     res.send(`<h1>Welcome to ${subreddit} subreddit</h1>`);
-})
+});
 
-app.get('/r/:subreddit/:filter',(req,res) => {
+app.get("/r/:subreddit/:filter", (req, res) => {
     const { subreddit, filter } = req.params;
     res.send(`<h1>You are at the ${subreddit} subreddit</h1>
     <h2>And You are watching ${filter} posts</h2>`);
-})
+});
 
-app.get('/cats', (req, res) => {
+app.get("/cats", (req, res) => {
     console.log("We got a new request!");
     res.send("We got your request! This is the response");
 });
 
-app.get('/dogs', (req, res) => {
+app.get("/dogs", (req, res) => {
     console.log("We got a new request!");
     res.send("<h1>We got your request! This is the response</h1>");
 });
 
-app.get('*', (req, res) => {
+app.get("*", (req, res) => {
     console.log("We got a new request!");
     res.send("404 Not found");
 });
 
 app.listen(3000, () => console.log("Listening on port 3000"));
+
+// Sneaky commit ;)
